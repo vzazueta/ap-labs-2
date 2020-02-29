ce10ced774cdb8d76dfb2842348c21103214a28f
V Zazueta <A00226702@itesm.mx>
2020-02-29 12:02:08 -0600

    Month Day
    Signed-off-by: V Zazueta <A00226702@itesm.mx>

--- labs/month-day/month_day.c
+++ labs/month-day/month_day.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* month_day function's prototype*/
 void month_day(int year, int yearday, int *pmonth, int *pday);
 
-int main() {
+int main(int argc, char **argv) {
+    int month, day;
+    int year = strtol(argv[1], NULL, 10), yearday = strtol(argv[2], NULL, 10);
+    char *monthName;
+    char *namesOfMonths[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    month_day(year, yearday, &month, &day);
+
+    monthName = namesOfMonths[month];
+
+    printf("%s %d, %d\n", monthName, day, year);
+
     return 0;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday){
+    int leap = (year%4 == 0) && (year%100 != 0) || (year%400 == 0);
+    int month, day, i;
+    int daysInMonths[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    if(leap){
+      daysInMonths[1] += 1;
+      
+      for(i=0; i<12; i++){
+        if(yearday <= daysInMonths[i]) {
+          break;
+        }
+        yearday -= daysInMonths[i];
+      }
+    }
+    else{
+      for(i=0; i<12; i++){
+        if(yearday <= daysInMonths[i]) {
+          break;
+        }
+        yearday -= daysInMonths[i];
+      }
+    }
+    
+    *pday = yearday;
+    *pmonth = i;
 }
