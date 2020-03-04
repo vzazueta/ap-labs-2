a56ca8b592f0255e050d20b02782514863b398b6
V Zazueta <A00226702@itesm.mx>
2020-03-03 21:26:16 -0600

    Slices & Maps
    Signed-off-by: V Zazueta <A00226702@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	maps := make(map[string]int)
+	arr := strings.Fields(s)
+	for _, i := range arr {
+		maps[i]++
+	}
+	return maps
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,22 @@
 package main
 
-func main() {
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	ady := make([][]uint8, dy)
+	adx := make([]uint8, dx)
+	
+	for x := range ady {
+	 for y := range adx {
+   	  adx[y] = uint8((x+y)/2)
+	    }
+	 ady[x] = adx
+	 }
+
+	return ady
 }
+
+func main() {
+	pic.Show(Pic)
+}
+
