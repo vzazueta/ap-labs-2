303033ccb7448cf392e9754f9e356ab068f95797
V Zazueta <A00226702@itesm.mx>
2020-03-17 12:43:20 -0600

    First Partial A00226702
    Signed-off-by: V Zazueta <A00226702@itesm.mx>

--- challenges/first-partial/dir-scan.go
+++ challenges/first-partial/dir-scan.go
@@ -3,11 +3,92 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
-// scanDir stands for the directory scanning implementation
+var count [5]int
+var leng [6]int
+var spaces [6]string
+
 func scanDir(dir string) error {
-	return nil
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		switch mode := info.Mode(); {
+		case mode.IsDir():
+			count[0]++
+		case mode&os.ModeSymlink != 0:
+			count[1]++
+		case mode&os.ModeDevice != 0:
+			count[2]++
+		case mode&os.ModeSocket != 0:
+			count[3]++
+		default:
+			count[4]++
+		}
+
+		return nil
+	})
+
+	leng[0] = len(dir)
+
+	for i := 1; i < 6; i++ {
+		leng[i] = digCount(count[i-1])
+	}
+
+	max := Max(leng)
+	var hyphens string
+
+	for i := 0; i < max; i++ {
+		hyphens += "-"
+	}
+
+	for i := 1; i < 6; i++ {
+		var sp string
+		var spanum int
+		spanum = max - leng[i]
+
+		for j := 0; j < spanum; j++ {
+			sp += " "
+		}
+
+		spaces[i] = sp
+	}
+
+	fmt.Printf("+-------------------------+--%s+\n", hyphens)
+	fmt.Printf("| Path                    | %s%s |\n", dir, spaces[0])
+	fmt.Printf("+-------------------------+--%s+\n", hyphens)
+	fmt.Printf("| Directories             | %d%s |\n", count[0], spaces[1])
+	fmt.Printf("| Symbolic Links          | %d%s |\n", count[1], spaces[2])
+	fmt.Printf("| Devices                 | %d%s |\n", count[2], spaces[3])
+	fmt.Printf("| Sockets                 | %d%s |\n", count[3], spaces[4])
+	fmt.Printf("| Other files             | %d%s |\n", count[4], spaces[5])
+	fmt.Printf("+-------------------------+--%s+\n", hyphens)
+
+	return err
+}
+
+func Max(a [6]int) (max int) {
+	max = a[0]
+	for _, value := range a {
+		if value > max {
+			max = value
+		}
+	}
+	return max
+}
+
+func digCount(number int) int {
+	count := 0
+	
+	if number == 0 {
+		return 1
+	}	
+	
+	for number != 0 {
+		number /= 10
+		count += 1
+	}
+	return count
 }
 
 func main() {
@@ -17,5 +98,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	scanDir(os.Args[1])
+	dir := os.Args[1]
+	err := scanDir(dir)
+
+	if err != nil {
+		fmt.Printf("Error scanning directory %s: %v", dir, err)
+		os.Exit(1)
+	}
 }
--- challenges/first-partial/pacman-analyzer.c
+++ challenges/first-partial/pacman-analyzer.c
@@ -1,25 +1,346 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+//#include <io.h>
 
 #define REPORT_FILE "packages_report.txt"
 
-void analizeLog(char *logFile, char *report);
+void analyzeLog(char *logFile, char *report);
+
+struct file{
+      char name[64];
+      char installDate[20];
+      char upgradeDate[20];
+      char removeDate[20];
+      int upgrades;
+};
+
+struct file files[2048];
 
 int main(int argc, char **argv) {
 
+int p;
+
+for(p=0; p<2048; p++){
+      files[p].name[0] = '\0';
+      files[p].installDate[0] = '\0';
+      files[p].upgradeDate[0] = '\0';
+      files[p].removeDate[0] = '\0';
+      files[p].upgrades = 0;
+}
+
+
     if (argc < 2) {
-	printf("Usage:./pacman-analizer.o pacman.log\n");
-	return 1;
+        printf("Usage:./pacman-analizer.o pacman.log\n");
+        return 1;
     }
 
-    analizeLog(argv[1], REPORT_FILE);
+    analyzeLog(argv[1], REPORT_FILE);
 
     return 0;
 }
 
-void analizeLog(char *logFile, char *report) {
-    printf("Generating Report from: [%s] log file\n", logFile);
+void analyzeLog(char *logFile, char *report) {
+      printf("Generating Report from: [%s] log file\n", logFile);
 
-    // Implement your solution here.
+      char *file;
+      int fd, i;
+      int characters;
+      char buffer[1], line[1024];
+      int p;
+      int upgraded, removed, installed;
+      i = upgraded = removed = installed = 0;
+      file = logFile;
 
-    printf("Report is generated at: [%s]\n", report);
+      fd = open(file, O_RDONLY, 0600);
+
+      if(fd == -1){
+            printf("Unreadable");
+            exit(1);
+      }
+
+      int lastInstalledIndex = 0;
+      characters = read(fd, buffer, 1);
+
+      for(;;)
+      {
+            characters = read(fd, buffer, 1);
+            
+            if(characters == -1){
+                  printf("Error");
+                  exit(1);
+                  break;
+            }
+            else if(characters == 0) break;
+
+            if(buffer[0] != '\n'){
+                  line[i] = buffer[0];
+                  i++;
+                 
+            }
+            else{
+                  line[i++] = '\n';
+                  line[i] = '\0';
+                  i=0;
+                  int j=0;
+                  char *result;
+                  
+                  if((result = strstr(line, "installed")) != NULL) {
+                        char *doi, *position = strstr(result, " ")+1;
+                        char nameOfProgram[64];
+                        char dateOfInstall[20];
+
+                        while(position[j] != ' '){
+                              nameOfProgram[j] = position[j];
+                              j++;
+                        }
+
+                        nameOfProgram[j] = '\0';
+                        j=0;
+
+                        while(files[j].name[0] != '\0'){
+                              if(strcmp(files[j].name, nameOfProgram) == 0) {
+                                    j = -1;
+                                    break;
+                              }
+                              
+                              j++;
+                        }
+
+                        if(j >= 0){
+                              strcpy(files[j].name, nameOfProgram);
+                              doi = strstr(line, "[")+1;
+                              int k=0;
+
+                              while(doi[k] != ']'){
+                                    dateOfInstall[k] = doi[k];
+                                    k++;
+                              }
+
+                              dateOfInstall[k] = '\0';
+                              strcpy(files[j].installDate, dateOfInstall);
+                              lastInstalledIndex = j;
+
+                        }
+                        
+                        installed++;  
+                  }
+                  else if((result = strstr(line, "removed")) != NULL) {
+                        char *dor, *position = strstr(result, " ")+1;
+                        char nameOfProgram[64];
+                        char dateOfRemove[20];
+                        
+                        while(position[j] != ' '){
+                              nameOfProgram[j] = position[j];
+                              j++;
+                        }
+                        nameOfProgram[j] = '\0';
+                        j=0;
+                        int comparison;
+
+                        while(files[j].name[0] != '\0'){
+                              if((comparison = strcmp(files[j].name, nameOfProgram)) == 0) {
+                                    break;
+                              }
+
+                              j++;
+                        }
+
+                        if(comparison == 0){
+                              dor = strstr(line, "[")+1;
+                              int k=0;
+
+                              while(dor[k] != ']'){
+                                    dateOfRemove[k] = dor[k];
+                                    k++;
+                              }
+
+                              dateOfRemove[k] = '\0';
+                              strcpy(files[j].removeDate, dateOfRemove);
+                             
+                        }
+                        
+                        removed++;
+                  }
+                  else if((result = strstr(line, "upgraded")) != NULL) {
+                        char *dou, *position = strstr(result, " ")+1;
+                        char nameOfProgram[64];
+                        char dateOfUpgrade[20];
+                        
+                        while(position[j] != ' '){
+                              nameOfProgram[j] = position[j];
+                              j++;
+                        }
+                        nameOfProgram[j] = '\0';
+                        j=0;
+                        int comparison;
+
+                        while(files[j].name[0] != '\0'){
+                              if((comparison = strcmp(files[j].name, nameOfProgram)) == 0) {
+                                    break;
+                              }
+
+                              j++;
+                        }
+
+                        if(comparison == 0){
+                              dou = strstr(line, "[")+1;
+                              int k=0;
+
+                              while(dou[k] != ']'){
+                                    dateOfUpgrade[k] = dou[k];
+                                    k++;
+                              }
+
+                              dateOfUpgrade[k] = '\0';
+                              strcpy(files[j].upgradeDate, dateOfUpgrade);
+                              
+                              files[j].upgrades++;
+                        }
+                        
+                        upgraded++;
+                     
+                  }
+
+            }
+
+      }
+
+      int g;
+      for(g = (lastInstalledIndex+1); p<2048; p++){
+            files[g].name[0] = '\0';
+            files[g].installDate[0] = '\0';
+            files[g].upgradeDate[0] = '\0';
+            files[g].removeDate[0] = '\0';
+            files[g].upgrades = 0;
+      }
+
+      if(close(fd) == -1){
+            
+            printf("Unreadable");
+            exit(1);
+      }
+
+      file = report;
+      fd = open(file, O_CREAT | O_WRONLY, 0600);
+
+      if(fd == -1){
+            printf("Unwritable");
+            exit(1);
+      }
+
+      size_t max_size = 1000;
+      char input[max_size];
+      int currentInstalled = (installed-removed);
+      int j;
+
+      strcpy(input, "Pacman Packages Report\n");
+      characters = write(fd, input, strlen(input));
+
+      for(j=0; j<8; j++){
+
+            if(characters == -1){
+                        printf("Error");
+                        exit(1);
+                        break;
+            }
+            else if(characters == 0) break;
+
+            switch(j){
+                  case 0:
+                  strcpy(input, "----------------------\n");
+                  characters = write(fd, input, strlen(input));
+                  break;
+
+                  case 1:
+                  snprintf(input, max_size, "- Installed packages : %d\n", installed);
+                  characters = write(fd, input, strlen(input));
+                  break;
+
+                  case 2:
+                  snprintf(input, max_size, "- Removed packages   : %d\n", removed);
+                  characters = write(fd, input, strlen(input));
+                  break;
+
+                  case 3:
+                  snprintf(input, max_size, "- Upgraded packages  : %d\n", upgraded);
+                  //input = "- Upgraded packages  : %d\n";
+                  characters = write(fd, input, strlen(input));
+                  break;
+
+                  case 4:
+                  snprintf(input, max_size, "- Current installed  : %d\n", currentInstalled);
+                  characters = write(fd, input, strlen(input));
+                  break;
+
+                  case 5:
+                  strcpy(input, "\n");
+                  characters = write(fd, input, strlen(input));
+                  break;
+
+                  case 6:
+                  strcpy(input, "List of packages\n");
+                  characters = write(fd, input, strlen(input));
+                  break;
+
+                  case 7:
+                  strcpy(input, "----------------\n");
+                  characters = write(fd, input, strlen(input));
+                  break;
+            }
+
+      }
+
+      j=0;
+
+      char upD[20], remD[20];
+      int fileArraySize;
+
+      for(fileArraySize=0; fileArraySize<2048; fileArraySize++){
+            if(files[fileArraySize].name[0] == '\0') break;
+      }
+      
+
+      for(j=0; j<fileArraySize; j++){
+            if(strlen(files[j].upgradeDate) ==  0){
+                  strcpy(upD, "-");
+                  upD[1] = '\0';
+            }
+            else{
+                  strcpy(upD, files[j].upgradeDate);
+            }
+
+            if(strlen(files[j].removeDate) ==  0){
+                  strcpy(remD, "-");
+                  remD[1] = '\0';
+            }
+            else{
+                  strcpy(remD, files[j].upgradeDate);
+            }
+
+            if(files[j].name[0] != '\0'){
+            snprintf(input, max_size, 
+            "- Package Name         : %s\n"
+            "   - Install date      : %s\n"
+            "   - Last update date  : %s\n"
+            "   - How many updates  : %d\n"
+            "   - Removal date      : %s\n",
+               files[j].name, files[j].installDate, upD, files[j].upgrades, remD);
+           
+            }
+            characters = write(fd, input, strlen(input));
+            
+      }
+      
+
+      if(close(fd) == -1){
+            printf("Unwritable");
+            exit(1);
+      }
+
+      printf("Report is generated at: [%s]\n", report);
 }
