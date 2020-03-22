559bfee8420fac52842d56eeb86eaa16ebece5d1
V Zazueta <A00226702@itesm.mx>
2020-03-21 22:39:27 -0600

    String Manipulation Homework
    Signed-off-by: V Zazueta <A00226702@itesm.mx>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
-int main() {
-    return 0;
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char** argv[]){
+      //printf("1");
+      char *argv1 = (char *)argv[1];
+      int argv1Length = mystrlen(argv1);
+      char *argv2 = (char *)argv[2];
+      char *argv3 = (char *)argv[3];
+      char *newStr = mystradd(argv1, argv2);
+      //printf("2");
+      char* found;
+      
+      if(mystrfind(newStr, argv3) == 1) {
+            found = "yes";
+      } else {
+            found = "no";
+      }
+
+      //printf("3");
+
+      printf("Initial Length     \t: %d\n", argv1Length);
+      //printf("4");
+      printf("New String         \t: %s\n", newStr);
+      printf("Substring was found\t: %s\n", found);
+
+      return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,47 @@
-
+#include <stdio.h>
 
 int mystrlen(char *str){
-    return 0;
+      int i=0;
+      
+      while((str[i]) != '\0'){
+            if((str[i] == '\n') || (str[i] == ' ') || (str[i] == '\t')) continue;
+            i++;
+      }
+
+      return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+      char* new = origin;
+      int i=0, j=0;
+
+      while((origin[i]) != '\0'){
+            new[i] = origin[i];
+            i++;
+      }
+      
+      while((addition[j]) != '\0'){
+            (new[i]) = (addition[j]);
+            i++;
+            j++;
+      }
+
+      return new;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+      int i=0, j=0;
+
+      while((origin[i]) != '\0'){
+            //printf("1");
+            if((origin[i]) == (substr[j])){
+                  while((origin[i++]) == (substr[j++])){
+                        //printf("%c, %c\n", (origin[i]), (substr[j]));
+                        if((substr[j]) == '\0') return 1;
+                  }
+            } else i++;
+            j=0;   
+      }
+      
+      return 0;
 }
