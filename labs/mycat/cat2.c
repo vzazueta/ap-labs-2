7a8f9a74d5a1a2bd55a862afb7bf0df41e92eedc
V Zazueta <A00226702@itesm.mx>
2020-03-25 20:50:29 -0600

    MyCat Lab A00226702
    Signed-off-by: V Zazueta <A00226702@itesm.mx>

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,38 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-/* filecopy:  copy file ifp to file ofp */
-void filecopy(FILE *ifp, FILE *ofp)
+int read2(int f){
+    char buffer[1];
+    int x;
+
+    x = read(f, buffer, 1);
+
+    if(x == 0){
+        return EOF;
+    } else if(x == -1) {
+        printf("Error Read\n");
+        exit(1);
+    }
+
+    return buffer[0];
+}
+
+void write2(int f, char c){
+    if(write(f, &c, 1) == -1){
+        printf("Error Write\n");
+        exit(1);
+    }
+}
+
+void filecopy(int ifp, int ofp)
 {
-    int c;
+    int read2(int);
+    void write2(int, char);
+    int character;
 
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
-
+    while((character = read2(ifp)) != EOF){
+        write2(ofp, character);
+    }
 }
 
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
-    char *prog = argv[0];   /* program name for errors */
-
-    if (argc == 1)  /* no args; copy standard input */
-        filecopy(stdin, stdout);
-    else
-        while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
-                fprintf(stderr, "%s: can′t open %s\n",
-			prog, *argv);
-                return 1;
-            } else {
-                filecopy(fp, stdout);
-                fclose(fp);
+    //printf("0");
+    int fd, i = 1;
+    void filecopy(int, int);
+    //char *prog = argv[0];   /* program name for errors */
+    //printf("1");
+    
+    
+    if (argc == 1){  /* no args; copy standard input */
+        //printf("2");
+        filecopy(fileno(stdin), fileno(stdout));
+    } else{
+        while (--argc > 0){
+            //printf("3");
+            if((fd = open(argv[i++], O_RDONLY, 0600)) == NULL){
+                printf("Null");
+            } else if (fd == -1){
+                printf("Error Open\n");
+                exit(1);
+            } else {            
+                filecopy(fd, fileno(stdout));
+                
+                if(close(fd) == -1 ){
+                    printf("Error Close\n");
+                    exit(1);
+                }
             }
-
-    if (ferror(stdout)) {
-        fprintf(stderr, "%s: error writing stdout\n", prog);
-        return 2;
+        }
     }
 
     return 0;
