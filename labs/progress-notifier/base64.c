4442a869299278eac08093e3c5b3342669e9fbc8
V Zazueta <A00226702@itesm.mx>
2020-05-15 23:54:56 -0500

    Base64
    Signed-off-by: V Zazueta <A00226702@itesm.mx>

--- labs/progress-notifier/base64.c
+++ labs/progress-notifier/base64.c
@@ -1,7 +1,200 @@
+    /***********************************************************
+    * Base64 library implementation                            *
+    * @author Ahmed Elzoughby                                  *
+    * @date July 23, 2017                                      *
+    *                                                          *
+    * Link:                                                    *
+    * https://github.com/elzoughby/Base64/blob/master/base64.c *
+    *                                                          *
+    ***********************************************************/
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
 #include "logger.h"
 
-int main(){
-    // Place your magic here
+#define MAX_SIZE 102400
+
+int fileSize;
+int advanceRate;
+int start = 0;
+int progress = 0;
+pid_t pid;
+char base46_map[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                     'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+
+char *base64_encode(char *);
+char *base64_decode(char *);
+
+void sigusr1_handler(int);
+void sigint_handler(int);
+
+int main(int argc, char **argv){
+      int rd;
+      FILE *fileRead, *fileWrite;
+      char *instruction, *filename;
+      char input[MAX_SIZE];
+      pid = getpid();
+
+      signal(SIGUSR1, sigusr1_handler);
+      signal(SIGINT, sigint_handler);
+
+      instruction = argv[1];
+      filename = argv[2];
+
+      fileRead = fopen(filename, "r");
+
+      rd = fread(input, MAX_SIZE+1, 1, fileRead);
+      if(rd == -1){
+            errorf("Error in read()");
+            exit(1);
+      }
+
+      fseek(fileRead, 0, SEEK_END);
+      fileSize = ftell(fileRead);
+      fseek(fileRead, 0, SEEK_SET);
+
+      advanceRate = fileSize/100;
+
+      //printf("\nfileSize: %d\nadvanceRate: %d\n", fileSize, advanceRate);
+
+      if(strcmp("--encode", instruction) == 0){
+            fileWrite = fopen("encoded.txt", "w");
+            
+            //kill(pid, SIGUSR1);
+            //sleep(1);
+            start++;
+            fwrite( base64_encode(input), 1 , MAX_SIZE, fileWrite );
+            
+            //sleep(1);
+            //kill(pid, SIGUSR1);
+            
+            printf("\n");
+
+      } else if(strcmp("--decode", instruction) == 0){
+            fileWrite = fopen("decoded.txt", "w");
+            
+            //kill(pid, SIGUSR1);
+            //sleep(1);
+            start++;
+            fwrite( base64_decode(input), 1 , MAX_SIZE, fileWrite );
+            //sleep(1);
+            //kill(pid, SIGUSR1);
+
+            printf("\n");
+
+      } else {
+            errorf("Incorrect format");
+            exit(1);
+      }
+
     return 0;
+}
+
+
+
+char* base64_encode(char* plain) {
+      char counts = 0;
+      char buffer[3];
+      char* cipher = malloc(strlen(plain) * 4 / 3 + 4);
+      int i = 0, c = 0;
+      
+      int advancement = advanceRate-1;
+
+      for(i = 0; plain[i] != '\0'; i++) {
+            
+            if(i == advancement){
+                  advancement += advanceRate;
+                  progress++;
+                  sleep(1);
+            }
+            
+            buffer[counts++] = plain[i];
+            if(counts == 3) {
+                  cipher[c++] = base46_map[buffer[0] >> 2];
+                  cipher[c++] = base46_map[((buffer[0] & 0x03) << 4) + (buffer[1] >> 4)];
+                  cipher[c++] = base46_map[((buffer[1] & 0x0f) << 2) + (buffer[2] >> 6)];
+                  cipher[c++] = base46_map[buffer[2] & 0x3f];
+                  counts = 0;
+            }
+      }
+     
+      if(counts > 0) {
+            cipher[c++] = base46_map[buffer[0] >> 2];
+            if(counts == 1) {
+                  cipher[c++] = base46_map[(buffer[0] & 0x03) << 4];
+                  cipher[c++] = '=';
+            } else {                      // if counts == 2
+                  cipher[c++] = base46_map[((buffer[0] & 0x03) << 4) + (buffer[1] >> 4)];
+                  cipher[c++] = base46_map[(buffer[1] & 0x0f) << 2];
+            }
+            cipher[c++] = '=';
+      }
+
+      cipher[c] = '\0';   /* string padding character */
+      return cipher;
+}
+
+
+char* base64_decode(char* cipher) {
+      char counts = 0;
+      char buffer[4];
+      char* plain = malloc(strlen(cipher) * 3 / 4);
+      int i = 0, p = 0;
+      
+      int advancement = advanceRate-1;
+
+      for(i = 0; cipher[i] != '\0'; i++) {
+            
+            if(i == advancement){
+                  advancement += advanceRate;
+                  progress++;
+                  sleep(1);
+            }
+            char k;
+
+            for(k = 0 ; k < 64 && base46_map[k] != cipher[i]; k++);
+            buffer[counts++] = k;
+            if(counts == 4) {
+                  plain[p++] = (buffer[0] << 2) + (buffer[1] >> 4);
+                  if(buffer[2] != 64)
+                  plain[p++] = (buffer[1] << 4) + (buffer[2] >> 2);
+                  if(buffer[3] != 64)
+                  plain[p++] = (buffer[2] << 6) + buffer[3];
+                  counts = 0;
+            }
+      }
+
+      plain[p] = '\0';    /* string padding character */
+      return plain;
+}
+
+void sigusr1_handler(int signum)
+{
+      if ((signum == SIGUSR1) && (start == 0)) {
+            write(STDOUT_FILENO, "Beginning Process!\n\0", 20);
+      } else if((signum == SIGUSR1) && (start == 1) && (progress == 101)){
+            write(STDOUT_FILENO, "Finished!\n\0", 20);
+      } else {
+            char buffer[20];      
+            sprintf(buffer, "Progress: %d%%\n", progress);
+            usleep(1);
+            write(STDOUT_FILENO, buffer, 20);
+
+      }
+}
+
+void sigint_handler(int signum)
+{
+    if (signum == SIGINT)
+    {
+      write(STDOUT_FILENO, "\nProcess was interrupted\n\n", 30);
+      exit(1);
+    }
 }
--- labs/progress-notifier/logger.c
+++ labs/progress-notifier/logger.c
@@ -1,23 +1,119 @@
 #include <stdio.h>
-#include "logger.h"
+#include <stdarg.h>	
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <syslog.h>
+
+#define RESET		0
+#define BRIGHT 		1
+#define DIM			2
+#define UNDERLINE 	3
+#define BLINK		4
+#define REVERSE		7
+#define HIDDEN		8
+
+#define BLACK 		0
+#define RED			1
+#define GREEN		2
+#define YELLOW		3
+#define BLUE		4
+#define MAGENTA		5
+#define CYAN		6
+#define WHITE		7
+
+int logTypeBoolean = 0;
+
+void textcolor(int attr, int fg, int bg)
+{	char command[13];
+
+	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+	printf("%s", command);
+}
 
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
-    return 0;
+	
+	if((strcmp(logType, "stdout") == 0) || (strcmp(logType, "") == 0)){
+		printf("Initializing Logger on: %s\n", "stdout");
+		logTypeBoolean = 0;    
+	} else if(strcmp(logType, "syslog") == 0){
+		printf("Initializing Logger on: %s\n", "syslog");
+		logTypeBoolean = 1;    
+	} else {
+		printf("Incorrect log type");
+	}
+    	
+	
 }
 
-int infof(const char *format, ...) {
-    return 0;
+int infof(char *format, ...){
+      va_list arg; 
+	va_start(arg, format);
+	textcolor(BRIGHT, BLUE, BLACK);
+	
+	if(logTypeBoolean == 0){
+		vprintf(format, arg);
+	} else if(logTypeBoolean == 1){
+		vsyslog(LOG_INFO, format, arg);
+	}
+
+	textcolor(RESET, WHITE, BLACK);	
+	va_end(arg);
+
+	return 4;
 }
 
-int warnf(const char *format, ...) {
-    return 0;
+int warnf(char *format, ...){
+      va_list arg; 
+	va_start(arg, format);
+	textcolor(BRIGHT, YELLOW, BLACK);
+	
+	if(logTypeBoolean == 0){
+		vprintf(format, arg);
+	} else if(logTypeBoolean == 1){
+		vsyslog(LOG_WARNING, format, arg);
+	}
+	
+	textcolor(RESET, WHITE, BLACK);
+      va_end(arg);
+
+	return 5;
 }
 
-int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
-    return 0;
+int errorf(char *format, ...){
+      va_list arg; 
+	va_start(arg, format);
+	textcolor(BRIGHT, RED, BLACK);
+	
+	if(logTypeBoolean == 0){
+		vprintf(format, arg);
+	} else if(logTypeBoolean == 1){
+		vsyslog(LOG_ERR, format, arg);
+	}
+	
+	textcolor(RESET, WHITE, BLACK);
+      va_end(arg);	
+
+	return 6;
 }
 
+int panicf(char *format, ...){
+      va_list arg; 
+	va_start(arg, format);
+	textcolor(BRIGHT, WHITE, RED);
+	
+	if(logTypeBoolean == 0){
+		vprintf(format, arg);
+            fflush(stdout);
+	} else if(logTypeBoolean == 1){
+		vsyslog(LOG_EMERG, format, arg);
+            //fflush(syslog);
+	}
+	
+	textcolor(RESET, WHITE, BLACK);
+      va_end(arg);	
+
+      raise(SIGABRT);
+
+	return 666;
+}
