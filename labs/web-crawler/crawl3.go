1ec18ca005ce26c88f88e191a9f2be60aa563db7
V Zazueta <A00226702@itesm.mx>
2020-05-17 01:57:49 -0500

    Web Crawler
    Signed-off-by: V Zazueta <A00226702@itesm.mx>

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -1,29 +1,21 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 241.
-
-// Crawl2 crawls web links starting with the command-line arguments.
-//
-// This version uses a buffered channel as a counting semaphore
-// to limit the number of concurrent calls to links.Extract.
-//
-// Crawl3 adds support for depth limiting.
-//
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 
-	"gopl.io/ch5/links"
+	"github.com/adonovan/gopl.io/ch5/links"
 )
 
 //!+sema
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests.
-var tokens = make(chan struct{}, 20)
+var (
+	tokens = make(chan struct{}, 20)
+	seen   = make(map[string]bool)
+)
 
 func crawl(url string) []string {
 	fmt.Println(url)
@@ -37,31 +29,46 @@ func crawl(url string) []string {
 	return list
 }
 
-//!-sema
-
-//!+
-func main() {
-	worklist := make(chan []string)
-	var n int // number of pending sends to worklist
-
-	// Start with the command-line arguments.
-	n++
-	go func() { worklist <- os.Args[1:] }()
-
-	// Crawl the web concurrently.
-	seen := make(map[string]bool)
-	for ; n > 0; n-- {
-		list := <-worklist
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
-			}
-		}
+func crawlRec(depth int, url string, finish chan bool) {
+	if depth == 0 {
+		finish <- true
+		return
 	}
+
+	visited, confirm := seen[url]
+	if visited && confirm {
+		finish <- true
+		return
+	} else {
+		seen[url] = true
+	}
+
+	urls := crawl(url)
+
+	urlsFinished := make(chan bool)
+	for _, link := range urls {
+		go crawlRec(depth-1, link, urlsFinished)
+		<-urlsFinished
+	}
+
+	finish <- true
 }
 
-//!-
+func main() {
+
+	if len(os.Args) != 3 {
+		fmt.Println("Incorrect Format")
+		return
+	}
+
+	depth := flag.Int("depth", 1, "crawl until limit depth")
+	flag.Parse()
+
+	url := os.Args[2]
+	seen[url] = false
+
+	done := make(chan bool)
+	go crawlRec(*depth, url, done)
+	<-done
+}
+
